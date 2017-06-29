753df02a71d58b0639334713e37420b1611e6580
SZK <shenzhenke@foxmail.com>
2017-06-29 16:27:18 +0800

    数组出现次数超一半+二维数组的查找问题

--- arrayoperation/arrayoperation/arrayoperation.h
+++ arrayoperation/arrayoperation/arrayoperation.h
@@ -126,31 +126,117 @@
 //给定一个红包的金额数组gifts及它的大小n，请返回所求红包的金额。
 //若没有金额超过总数的一半，返回0。
 
-class Gift 
-{
+//class Gift 
+//{
+//public:
+//	int getValue(vector<int> gifts, int n)
+//	{
+//		// write code here
+//		map<int, int> countGifts;
+//		int giftsNumber = gifts.size();
+//		for (int i = 0; i<giftsNumber; i++)
+//		{
+//			countGifts[gifts[i]]++;
+//		}
+//		int m = n / 2;
+//		map<int, int>::iterator it = countGifts.begin();
+//		while (it != countGifts.end())
+//		{
+//			if ((it->second)>m)
+//				return it->first;
+//			it++;
+//		}
+//		return 0;
+//	}
+//};
+
+
+
+//在一个二维数组中，每一行都按照从左到右递增的顺序排序，每一列都按照从上到下递增的顺序排序。
+//请完成一个函数，输入这样的一个二维数组和一个整数，判断数组中是否含有该整数
+
+//class Solution
+//{
+//
+//public:
+//	bool Find(int target, vector<vector<int> > array)
+//	{
+//		size_t row = array.size();      //行
+//		size_t col = array[0].size();  // 列 
+//		for (int i = row - 1; i >= 0; i--)
+//		{
+//			if (array[i][col - 1] == target)
+//				return true;
+//		    if (array[i][col - 1]>target)
+//			{
+//				//二分查找
+//				int end = col - 1;
+//				int start = 0;
+//				while (start < end)
+//				{
+//					int mid = start + (end - start) / 2;
+//					if (array[i][mid] > target)
+//						end = mid ;
+//					else if (array[i][mid] < target)
+//						start = mid + 1;
+//					else
+//						return true;
+//				}
+//			}
+//		}
+//		return false;
+//	}
+//};
+
+//class Solution
+//{
+//
+//public:
+//	bool Find(int target, vector<vector<int> > array)
+//	{
+//		size_t row = array.size();      //行
+//		size_t col = array[0].size();  // 列 
+//		for (int i = row - 1; i >= 0; i--)
+//		{
+//			if (array[i][col - 1] == target)
+//				return true;
+//			if (array[i][col - 1] > target)
+//			{
+//				//二分查找
+//				int end = col - 1;
+//				int start = 0;
+//				while (start < end)
+//				{
+//					int mid = start + (end - start) / 2;
+//					if (array[i][mid] > target)
+//						end = mid;
+//					else if (array[i][mid] < target)
+//						start = mid + 1;
+//					else
+//						return true;
+//				}
+//			}
+//		}
+//		return false;
+//	}
+//};
+
+
+//数组中有一个数字出现的次数超过数组长度的一半，请找出这个数字。
+//例如输入一个长度为9的数组{ 1, 2, 3, 2, 2, 2, 5, 4, 2 }。
+//由于数字2在数组中出现了5次，超过数组长度的一半，因此输出2。如果不存在则输出0。
+class Solution {
 public:
-	int getValue(vector<int> gifts, int n)
+	int MoreThanHalfNum_Solution(vector<int> numbers) 
 	{
-		// write code here
-		map<int, int> countGifts;
-		int giftsNumber = gifts.size();
-		for (int i = 0; i<giftsNumber; i++)
+		int Size = numbers.size();
+		map<int, int> countNumber;
+		for (int i = 0; i < Size; i++)
 		{
-			countGifts[gifts[i]]++;
-		}
-		int m = n / 2;
-		map<int, int>::iterator it = countGifts.begin();
-		while (it != countGifts.end())
-		{
-			if ((it->second)>m)
-				return it->first;
-			it++;
+			countNumber[numbers[i]]++;
+			if (countNumber[numbers[i]] >= (Size + 1) / 2)
+				return numbers[i];
 		}
 		return 0;
 	}
 };
-
-
-
-
-
--- arrayoperation/arrayoperation/test.cpp
+++ arrayoperation/arrayoperation/test.cpp
@@ -1,4 +1,4 @@
-#define _CRT_SECURE_NO_WARNINGS 
+﻿#define _CRT_SECURE_NO_WARNINGS 
 #include <iostream>
 #include <algorithm>
 #include <vector>
@@ -7,16 +7,64 @@ using namespace std;
 #include "arrayoperation.h"
 int main()
 {
+	//vector<int> v;
+	//v.push_back(1);
+	//v.push_back(2);
+	//v.push_back(2);
+	//v.push_back(2);
+	//v.push_back(2);
+	//v.push_back(2);
+	//v.push_back(10);
+	//v.push_back(3);
+	//Gift gift;
+	//cout << gift.getValue(v, v.size()) << endl;
+
+
+	//vector<vector<int> > array(5);
+	//for (int i = 0; i < 5; i++)
+	//{
+	//	for (int j = 0; j < 5; j++)
+	//	{
+	//		array[i].push_back((i+1)*(j+2));
+	//	}
+	//}
+	//for (int i = 0; i < 5; i++)
+	//{
+	//	for (int j = 0; j < 5; j++)
+	//	{
+	//		cout << array[i][j] << " ";
+	//	}
+	//	cout << endl;
+	//}
+	//Solution c;
+	//cout << c.Find(1, array) << endl;
+	//cout << c.Find(2, array) << endl;
+	//cout << c.Find(3, array) << endl;
+	//cout << c.Find(4, array) << endl;
+	//cout << c.Find(5, array) << endl;
+	//cout << c.Find(10, array) << endl;
+	//cout << c.Find(15, array) << endl;
+	//cout << c.Find(4, array) << endl;
+	//cout << c.Find(6, array) << endl;
+	//cout << c.Find(8, array) << endl;
+	//cout << c.Find(10, array) << endl;
+	//cout << c.Find(12, array) << endl;
+	//cout << c.Find(6, array) << endl;
+	//cout << c.Find(9, array) << endl;
+	//cout << c.Find(12, array) << endl;
+	//cout << c.Find(15, array) << endl;
+	//cout << c.Find(18, array) << endl;
+	//cout << c.Find(30, array) << endl;
+	//cout << c.Find(25, array) << endl;
+
+	Solution a;
+	int arr[] = { 1, 6, 2, 3, 4, 5, 1, 2, 1, 1 };
 	vector<int> v;
-	v.push_back(1);
-	v.push_back(2);
-	v.push_back(2);
-	v.push_back(2);
-	v.push_back(2);
-	v.push_back(2);
-	v.push_back(10);
-	v.push_back(3);
-	Gift gift;
-	cout << gift.getValue(v, v.size()) << endl;
+	for (size_t i = 0; i < sizeof(arr) / sizeof(arr[0]); i++)
+	{
+		v.push_back(arr[i]);
+	}
+	cout << a.MoreThanHalfNum_Solution(v) << endl;
+
 	return 0;
 }
